52382e356ea2e4b7f321e250891a0d259e92b590
Thibault Polge <thibault@thb.lt>
2017-10-09 00:01:04 +0200

    Test

--- roles/desktop.nix
+++ roles/desktop.nix
@@ -2,9 +2,17 @@
 
   imports = [ "/etc/nixos/roles/interactive.nix" ];
 
-  system.stateVersion = "17.03";
+  system.stateVersion = "17.09";
 
   services.pcscd.enable = true;
+
+  environment.systemPackages = [ pkgs.fwupd pkgs.glib_networking ];
+  systemd.packages = [ pkgs.fwupd ];
+
+  environment.variables =
+	{ GIO_EXTRA_MODULES = "${pkgs.glib_networking.out}/lib/gio/modules:${pkgs.gnome3.dconf}/lib/gio/modules";
+          GSETTINGS_SCHEMA_DIR = "${pkgs.gsettings_desktop_schemas}/share/gsettings-schemas/gsettings-desktop-schemas-3.22.0/glib-2.0/schemas";
+        };
 
 # Enable the X11 windowing system.
   services.xserver = {
@@ -30,6 +38,10 @@
     };
   };
 
-  time.timeZone = "Europe/Paris";
+  services.xserver.displayManager.lightdm.autoLogin = {
+    enable = true;
+    user = "thblt";
+  };
 
+  time.timeZone = "Europe/Paris";
 }
