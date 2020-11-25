59c381df7d86be0938537e3ae26dc1c1f055c809
Thibault Polge <thibault@thb.lt>
2020-11-25 19:28:14 +0100

    Use built-in hidpi support

--- configuration-maladict.nix
+++ configuration-maladict.nix
@@ -18,6 +18,8 @@
     ];
 
     console.font = pkgs.lib.mkForce "${pkgs.terminus_font}/share/consolefonts/ter-i32n.psf.gz"; # HiDPI console
+    # console.font = pkgs.lib.mkForce "${pkgs.terminus_font}/share/consolefonts/ter-i32n.psf.gz"; # HiDPI console
+    hardware.video.hidpi.enable = true;
 
     # Use the systemd-boot EFI boot loader.
     # TODO Find a way to move most of this to common.
