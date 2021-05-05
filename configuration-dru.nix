7837e834237ac82f5641d2b4edc50f30cb872484
Thibault Polge <thibault@thb.lt>
2021-05-05 20:42:13 +0200

    Simplify dru config

--- configuration-dru.nix
+++ configuration-dru.nix
@@ -21,19 +21,12 @@
       ./common.nix
     ];
 
-    # Use the systemd-boot EFI boot loader.
-    # TODO Find a way to move most of this to common.
-    boot.initrd.luks.devices = {
-      crypt = {
-        device = "/dev/nvme0n1p2";
-        allowDiscards = true;
-        preLVM = true;
-	    };
-    };
-
-    boot.kernelParams  = [ "acpi_rev_override=5" ];
-    # hardware.bumblebee.enable = true;
-
-    # Rainbow keyboard
-    hardware.ckb-next.enable = true;
-    }
+  # TODO Find a way to move most of this to common.
+  boot.initrd.luks.devices = {
+    crypt = {
+      device = "/dev/nvme0n1p2";
+      allowDiscards = true;
+      preLVM = true;
+	  };
+  };
+}
