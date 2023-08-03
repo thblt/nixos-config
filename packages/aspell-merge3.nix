b26c1ad53d2056f12622593942c6f08ed649cb69
Thibault Polge <thibault@thb.lt>
2023-08-03 22:21:20 +0200

    Simplify aspell-merge3 package

--- packages/aspell-merge3.nix
+++ packages/aspell-merge3.nix
@@ -1,31 +1,31 @@
 with (import <nixpkgs> {});
-let
-  f = { mkDerivation, base, bytestring, containers, lib }:
-    mkDerivation {
-      pname = "aspell-merge3";
-      version = "0.2.0.0";
-      src = fetchFromGitHub {
-        owner = "thblt";
-        repo = "aspell-merge3";
-        rev = "86541fbc80f3717ca185747808b1a09ac1cf1885";
-        sha256 = "sha256-iJOOTdtlMIIpcVHKFYUYsAp0XtGNcGMVffdi/Tcj7nU=";
-      };
-      isLibrary = false;
-      isExecutable = true;
-      executableHaskellDepends = with haskellPackages; [
-        base
-        bytestring
-        containers_0_6_7
-        optparse-applicative ];
-      description = "Automatic three-way merge for aspell personal dictionaries";
-      license = "unknown";
-      hydraPlatforms = lib.platforms.none;
-      };
+# let
+#   f = { mkDerivation, base, bytestring, containers, lib }:
+haskellPackages.mkDerivation {
+  pname = "aspell-merge3";
+  version = "0.2.0.0";
+  src = fetchFromGitHub {
+    owner = "thblt";
+    repo = "aspell-merge3";
+    rev = "86541fbc80f3717ca185747808b1a09ac1cf1885";
+    sha256 = "sha256-iJOOTdtlMIIpcVHKFYUYsAp0XtGNcGMVffdi/Tcj7nU=";
+  };
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = with haskellPackages; [
+    base
+    bytestring
+    containers_0_6_7
+    optparse-applicative ];
+  description = "Automatic three-way merge for aspell personal dictionaries";
+  license = "unknown";
+  hydraPlatforms = lib.platforms.none;
+}
 
-      haskellPackages = pkgs.haskellPackages;
-      variant = pkgs.lib.id;
+  # haskellPackages = pkgs.haskellPackages;
+  # variant = pkgs.lib.id;
 
-      drv = variant (haskellPackages.callPackage f {});
-in
+  # drv = variant (haskellPackages.callPackage f {});
+  # in
 
-if pkgs.lib.inNixShell then drv.env else drv
+  # if pkgs.lib.inNixShell then drv.env else drv
