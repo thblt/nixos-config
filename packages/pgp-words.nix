16f833148483368ef2cf7cecdcdfa2f3fa13d6e2
Thibault Polge <thibault@thb.lt>
2023-08-04 18:25:51 +0200

    Update pgp-words to 0.3.1

--- packages/pgp-words.nix
+++ packages/pgp-words.nix
@@ -3,21 +3,21 @@
 with (import <nixpkgs> {});
 rustPlatform.buildRustPackage rec {
   pname = "pgp-words";
-  version = "0.3.0.0";
+  version = "0.3.1";
   src = fetchFromGitHub {
     owner = "thblt";
     repo = "pgp-words.rs";
-    rev = "4f20806663051ab96125d6d72115793c3d0cc441";
+    rev = "47b0d8b7b1f46860e47cc0f4b74a2affc8a26fe7";
     # sha256 = lib.fakeHash; # To get the correct hash from the failed
     # build.
-    sha256 = "sha256-LJOaZJqc/3I+J/pOSi0noX/5BHElukeWIxIkeyFiFSc=";
+    sha256 = "sha256-JgSkiDCd3GJ+VYH3u/PY7bNSDsYgKebztS6cjdkrUfg=";
   };
-  cargoHash = "sha256-GRp6krNSs/bWcnDUp+PHIFiTvXIKZsaYhMkhGkCHosA=";
+  cargoHash = "sha256-PmgkgNcWy66kA3I73ZIMG5LgGmTV9a3gta6YQw7VbYg=";
 
   meta = with lib; {
-    description = "Make hex sequences easier to communicate over the phone.";
+    description = "Turn hex strings into simple words for easy verification.";
     license = licenses.gpl3Plus;
     homepage = "https://github.com/thblt/pgp-words.rs";
-    maintainers = [];
+    maintainers = [ nixpkgs.maintainers.thblt ];
   };
 }
