7d8ed2bfcfa89462aaa8fcb4a75d314c97ec1924
Thibault Polge <thibault@thb.lt>
2024-12-07 12:55:04 +0100

    Fix error

--- common.nix
+++ common.nix
@@ -49,7 +49,7 @@
   services.fstrim.enable = true;
 
   # Sound
-  hardware.pulseaudio.enable = ;
+  hardware.pulseaudio.enable = false;
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
 
