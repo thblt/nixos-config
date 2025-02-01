042527c1e9610b67ec0a60c26c088def452d4fda
Thibault Polge <thibault@thb.lt>
2025-02-01 10:16:13 +0100

    Formatting

--- flake.nix
+++ flake.nix
@@ -4,35 +4,35 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     helix.url = "github:helix-editor/helix/25.01";
-  rust-overlay = {
+    rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
-    };  };
+    };
+  };
 
   outputs = { nixpkgs, ... }@inputs: {
     # DRU (Thinkpad X270)
     nixosConfigurations.dru = nixpkgs.lib.nixosSystem {
-         system = "x86_64-linux";
-         modules = [
-           ./configuration-dru.nix
-           {nixpkgs.overlays = [
-              inputs.rust-overlay.overlays.default
-              inputs.helix.overlays.default
-            ];}
+      system = "x86_64-linux";
+      modules = [
+        ./configuration-dru.nix
+        {nixpkgs.overlays = [
+           inputs.rust-overlay.overlays.default
+           inputs.helix.overlays.default
+         ];}
 
-         ];
+      ];
     };
     # MARGOLOTTA
     nixosConfigurations.margolotta = nixpkgs.lib.nixosSystem {
-         system = "x86_64-linux";
-         modules = [
-           ./configuration-margolotta.nix
-           {nixpkgs.overlays = [
-              inputs.rust-overlay.overlays.default
-              inputs.helix.overlays.default
-            ];}
-
-         ];
+      system = "x86_64-linux";
+      modules = [
+        ./configuration-margolotta.nix
+        {nixpkgs.overlays = [
+           inputs.rust-overlay.overlays.default
+           inputs.helix.overlays.default
+         ];}
+      ];
     };
   };
 }
