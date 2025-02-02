f61b2ac1a3d48e161580f42eee26ba1e59102e89
Thibault Polge <thibault@thb.lt>
2025-02-02 13:32:20 +0100

    [margolotta] Various fixes

--- configuration-margolotta.nix
+++ configuration-margolotta.nix
@@ -15,29 +15,18 @@
 {
   networking.hostName = "margolotta";
 
-  imports =
-    [
-      ./hardware-configuration-margolotta.nix
-      ./common.nix
-    ];
+  imports = [ ./hardware-configuration-margolotta.nix ./common.nix ];
 
   # boot.kernelPackages = pkgs.linuxPackages_latest;
 
   boot.loader.systemd-boot = {
-    # BitLocker doesn't support boot chaining.  This sets the
-    # next-boot EFI variable to the Windows 11 loader and reboot when
-    # Win11 is selected.
-    extraInstallCommands = ''echo "reboot-for-bitlocker yes" >> /boot/loader/loader.conf'';
-    # v Pending PR #253260 v
-    # rebootForBitlocker = true;
+    rebootForBitlocker = true;
     configurationLimit = 10;
   };
 
   # Nvidia
-  graphics = {
-    enable = true;
-  };
-  hardware  = {
+  hardware = {
+    graphics.enable = true;
     nvidia = {
       package = config.boot.kernelPackages.nvidiaPackages.latest;
       open = true;
@@ -46,13 +35,13 @@
       powerManagement.enable = true;
     };
   };
-  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = [ "nvidia" ];
   programs.sway = {
     extraOptions = [ "--unsupported-gpu" ];
     # Note to self: this value is of type lines, so it will merge
     # automatically with the default in common.nix.
     extraSessionCommands = ''
-    export WLR_NO_HARDWARE_CURSORS=1
+      export WLR_NO_HARDWARE_CURSORS=1
     '';
   };
 
