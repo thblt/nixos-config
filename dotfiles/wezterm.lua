f74b3c6dabbb92359d250b9488cfdac7504a80d7
Thibault Polge <thibault@thb.lt>
2025-10-20 12:41:44 +0200

    [wezterm] Don't hardcode fish outside of MacOS

--- dotfiles/wezterm.lua
+++ dotfiles/wezterm.lua
@@ -5,7 +5,9 @@ config.hide_tab_bar_if_only_one_tab=true
 config.macos_fullscreen_extend_behind_notch = true
 config.font = wezterm.font "Fira Code"
 
-config.default_prog = { '/Users/thblt/.nix-profile/bin/fish', '-l' }
+if wezterm.target_triple == 'aarch64-apple-darwin' then
+    config.default_prog = { '/Users/thblt/.nix-profile/bin/fish', '-l' }
+end
 
 config.native_macos_fullscreen_mode = false
 config.macos_fullscreen_extend_behind_notch = true
